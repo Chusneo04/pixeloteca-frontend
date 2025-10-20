8208572268c77f1106160bafead6bf8f5d547213
Chusneo04 <hugoarroyogalle@gmail.com>
2025-10-20 09:00:40 +0200

    Diseño responsive de login finalizado

--- pixeloteca-frontend/src/App.js
+++ pixeloteca-frontend/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Inicio from './pages/inicioPage.js'
-
+import Login from './pages/loginPage.js';
 function App() {
   return (
     <Router>
       <Routes>
         <>
           <Route path="/" element={<Inicio />}/>
+          <Route path="/login" element={<Login />}/>
         </>
       </Routes>
     </Router>
