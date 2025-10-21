5d40cfb14a2f1b31c46ca99e781d4d0ad119ef29
Chusneo04 <hugoarroyogalle@gmail.com>
2025-10-21 11:19:43 +0200

    Register completado

--- pixeloteca-frontend/src/App.js
+++ pixeloteca-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Inicio from './pages/inicioPage.js'
+import Register from './pages/registerPage.js';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <>
           <Route path="/" element={<Inicio />}/>
+          <Route path="/register" element={<Register/>}/>
         </>
       </Routes>
     </Router>
---
+++ pixeloteca-frontend/src/pages/registerPage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+
+const Register = () => {
+
+    const [nombre, setNombre] = useState("")
+    const [email, setEmail] = useState("")
+    const [clave, setClave] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND}/register`, {
+                method:'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({nombre, email, clave})
+            })
+
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(data.message)
+            }
+            console.log(data);
+            window.location.href = '/perfil'
+            
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    return (
+        <div className="w-full min-h-screen bg-[#1C1C1E] flex flex-col items-center gap-[50px] pb-[50px] overflow-hidden">
+            <a href="/" className="text-[white] w-full flex ml-[50px] mt-[50px] text-[20px] underline">Volver a inicio</a>
+            <div className="flex flex-col gap-[20px]">
+                <h1 className="text-[white] font-extrabold text-[50px]">Inicio de sesión</h1>
+                <form onSubmit={handleSubmit} className="w-full flex flex-col items-center justify-center gap-[30px]">
+                    <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} placeholder="Nombre" className="bg-[white] outline-none w-[80%] h-[40px] rounded-[10px] p-[20px]"/>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Correo electrónico" className="bg-[white] outline-none w-[80%] h-[40px] rounded-[10px] p-[20px]"/>
+                    <input type="password" value={clave} onChange={(e) => setClave(e.target.value)} placeholder="Clave" className="bg-[white] outline-none w-[80%] h-[40px] rounded-[10px] p-[20px]"/>
+                    <p className="text-[white]">¿No tienes cuenta? <a href="/register" className="underline">Regístrate</a></p>
+                    <button type="submit" className="bg-[#F0A422] w-[8rem] h-[50px] text-[20px] rounded-[15px] flex items-center justify-center text-center cursor-pointer">Acceder</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
