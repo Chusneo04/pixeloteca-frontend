d6b057b8904c97d932da44dd4ceb841e9c0675a6
Chusneo04 <hugoarroyogalle@gmail.com>
2025-10-23 11:43:26 +0200

    Completada las interfaces de recuperar clave

--- pixeloteca-frontend/src/App.js
+++ pixeloteca-frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/inicioPage.js'
 import Login from './pages/loginPage.js';
 import Register from './pages/registerPage.js';
+import RecuperarClave from './pages/recuperarclavePage.js';
+import NuevaClave from './pages/nuevaclavePage.js';
 function App() {
   return (
     <Router>
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Inicio />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register/>}/>
+          <Route path="/recuperar_clave" element={<RecuperarClave />}/>
+          <Route path="/nueva_clave" element={<NuevaClave />}/>
         </>
       </Routes>
     </Router>
--- pixeloteca-frontend/src/pages/registerPage.js
+++ pixeloteca-frontend/src/pages/registerPage.js
@@ -22,8 +22,13 @@ const Register = () => {
             if (!response.ok) {
                 throw new Error(data.message)
             }
-            console.log(data);
-            window.location.href = '/perfil'
+
+            if (data.token !== undefined) {
+                localStorage.setItem('token', data.token)
+                window.location.href = '/perfil'
+            }
+
+            
             
         } catch (error) {
             alert(error.message);
@@ -33,13 +38,13 @@ const Register = () => {
     return (
         <div className="w-full min-h-screen bg-[#1C1C1E] flex flex-col items-center gap-[50px] pb-[50px] overflow-hidden">
             <a href="/" className="text-[white] w-full flex ml-[50px] mt-[50px] text-[20px] underline">Volver a inicio</a>
-            <div className="flex flex-col gap-[20px]">
-                <h1 className="text-[white] font-extrabold text-[50px]">Inicio de sesión</h1>
+            <div className="flex flex-col items-center justify-center gap-[20px] w-[25rem]">
+                <h1 className="text-[white] font-extrabold text-[50px]">Registro</h1>
                 <form onSubmit={handleSubmit} className="w-full flex flex-col items-center justify-center gap-[30px]">
                     <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} placeholder="Nombre" className="bg-[white] outline-none w-[80%] h-[40px] rounded-[10px] p-[20px]"/>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Correo electrónico" className="bg-[white] outline-none w-[80%] h-[40px] rounded-[10px] p-[20px]"/>
                     <input type="password" value={clave} onChange={(e) => setClave(e.target.value)} placeholder="Clave" className="bg-[white] outline-none w-[80%] h-[40px] rounded-[10px] p-[20px]"/>
-                    <p className="text-[white]">¿No tienes cuenta? <a href="/register" className="underline">Regístrate</a></p>
+                    <p className="text-[white]">¿No tienes cuenta? <a href="/login" className="underline">Inicia sesión</a></p>
                     <button type="submit" className="bg-[#F0A422] w-[8rem] h-[50px] text-[20px] rounded-[15px] flex items-center justify-center text-center cursor-pointer">Acceder</button>
                 </form>
             </div>
