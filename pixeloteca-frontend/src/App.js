ab03fb03bf6124c02afb56c130fb78df3ad7f5ba
Chusneo04 <hugoarroyogalle@gmail.com>
2025-10-29 09:35:01 +0100

    Creada la interfaz de perfil mostrando los titulos de manera responsive

--- pixeloteca-frontend/src/App.js
+++ pixeloteca-frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/loginPage.js';
 import Register from './pages/registerPage.js';
 import RecuperarClave from './pages/recuperarclavePage.js';
 import NuevaClave from './pages/nuevaclavePage.js';
+import Perfil from './pages/perfilPage.js';
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
           <Route path="/register" element={<Register/>}/>
           <Route path="/recuperar_clave" element={<RecuperarClave />}/>
           <Route path="/nueva_clave" element={<NuevaClave />}/>
+          <Route path="/perfil" element={<Perfil />} />
         </>
       </Routes>
     </Router>
